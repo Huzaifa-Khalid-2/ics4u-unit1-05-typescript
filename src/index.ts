7255dcdcf8e0d1085debf9937a8f6918e82b73e4
Huzaifa Khalid <huzaifa.khalid@mths.ca>
2022-09-28 13:51:23 +0000

    done

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,23 @@
 /**
+ * The program gets the wood length
+ *
+ * By:      Huzaifa Khalid
+ * Version: 1.0
+ * Since:   2022-9-28
+ */
+
+import promptSync from 'prompt-sync'
+
+/**
  * The function calculates the length of the wood
  *
  * @param {number} height  - height number
  * @param {number} width  - width number
  * @returns {number} Return value
  */
-function lengthCalculation(height: number, width: number): number {
+function lengthCalculator (height: number, width: number): number {
   const volume = 144
-  const length = volume / (height * width)
+  const length = volume / height / width
   return length
 }
 
@@ -16,13 +26,14 @@ try {
   const widthString = prompt('Enter the width(inch): ')
   const widthNumber = parseInt(widthString)
   const heightString = prompt('Enter the height(inch): ')
-  const heightNumber = parseInt(heightString)
+  const heightNumber = parseFloat(heightString)
   if (isNaN(+widthNumber) || isNaN(+heightNumber)) {
     throw new Error()
   }
-  const woodLength = lengthCalculation(heightNumber, widthNumber)
-  console.log(`The wood length should be ${woodLength} inch(es)`)
+  const woodLength = lengthCalculator(heightNumber, widthNumber)
+  console.log(`The wood should be ${woodLength} inch(es) long.`)
 } catch (e) {
-  console.log('\nInvalid. Input.')
+  console.log('\nInvalid Input.')
 }
+
 console.log('\nDone.')
